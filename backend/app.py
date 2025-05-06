44042ecfbebc41156d5d306c5943eb8bfce7ebd0
AGABA Mohamed <mohamed-amine.agaba@epitech.eu>
2025-05-06 07:57:45 +0200

    translated commentes from fr to en

--- backend/app.py
+++ backend/app.py
@@ -6,20 +6,20 @@ from routes import register_routes
 app = Flask(__name__)
 CORS(app)  # Allows frontend to access backend
 
-# Config de la base de données
+# Config of the database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost:5432/mesdb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# Initialisation de la base de données avec l'app Flask
+# initiate the database with flask
 db.init_app(app)
 
-# Enregistrement des routes
+# register routes
 register_routes(app)
 
-# Création des tables au démarrage
+# tables creation on startup
 with app.app_context():
     db.create_all()
 
-# Lancement du serveur
+# launch the app
 if __name__ == '__main__':
     app.run(debug=False, port=5000)
